b154bdbb40eadbcd23e1dd218c9b34ea1c91898d
Vladislav <vladislav@localhost.localdomain>
2023-06-01 15:24:36 +0800

    Added config files

---
+++ backend/cmd/learn/learn.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var message = "hello"
+	var variable string
+	variable = "hello"
+
+	fmt.Println(message)
+	fmt.Println(variable)
+	fmt.Println("Hello, go")
+}
--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,15 +6,26 @@ import (
 	"github.com/VladMak/auto_learn/internal/repository"
 	"github.com/VladMak/auto_learn/internal/service"
 	"log"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(domain.Server)
-	if err := srv.Run("8888", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatal("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
