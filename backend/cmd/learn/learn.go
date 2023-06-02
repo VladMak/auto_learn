9420c6b3f76b1c7387741faf595ebcaaf4a9797a
Vladislav <vladislav@localhost.localdomain>
2023-06-03 00:17:36 +0800

    Learning

--- backend/cmd/learn/learn.go
+++ backend/cmd/learn/learn.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func main() {
-	var message = "hello"
-	var variable string
-	variable = "hello"
+type Shape interface {
+	Area() float32
+}
 
-	fmt.Println(message)
-	fmt.Println(variable)
-	fmt.Println("Hello, go")
+type Square struct {
+	sideLength float32
+}
+
+func (s Square) Area() float32 {
+	return s.sideLength * s.sideLength
+}
+
+type Circle struct {
+	radius float32
+}
+
+func (c Circle) Area() float32 {
+	return float32(math.Pi) * c.radius * c.radius
+}
+
+func printShapeArea(s Shape) {
+	fmt.Printf("Площадь фигуры: %.2f см \n", s.Area())
+}
+
+func main() {
+	square := Square{10.5}
+	circle := Circle{5.7}
+
+	printShapeArea(square)
+	printShapeArea(circle)
 }
