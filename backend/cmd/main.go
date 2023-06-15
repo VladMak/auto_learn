f0de361a64ae8d50b5ffed7b71c882383d070c4e
Vladislav <vladislav@localhost.localdomain>
2023-06-15 12:34:33 +0800

    Adding graceful shutdown

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/VladMak/auto_learn/internal/service"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"syscall"
+	"os"
+	"os/signal"
+	"context"
 )
 
 func main() {
@@ -33,8 +37,26 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(domain.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatal("error occured while running http server: %s", err.Error())
+	go func () {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatal("error occured while running http server: %s", err.Error())
+		}
+	}()
+
+	logrus.Print("TodoApp Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<- quit
+
+	logrus.Print("TodoApp ShuttingDown")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured on db connection close: %s", err.Error())
 	}
 }
 
